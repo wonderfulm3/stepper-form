7ec5e300c8fe6146df1ed0a767727f9f4ab040ae
wonderfulm3 <tammen.bruccoleri@gmail.com>
2017-12-31 15:18:26 -0800

    More updates on subtext and style handling. More incoming later tonight. Happy NYE!

--- library/src/main/java/io/tammen/stepper/widget/MobileStepElement.java
+++ library/src/main/java/io/tammen/stepper/widget/MobileStepElement.java
@@ -11,6 +11,7 @@ import io.tammen.stepper.R;
 
 /**
  * Created by Tammen Bruccoleri on 12/30/2017.
+ *
  */
 
 public class MobileStepElement extends RelativeLayout implements View.OnClickListener {
@@ -31,14 +32,45 @@ public class MobileStepElement extends RelativeLayout implements View.OnClickLis
         super(context, attributeSet, defStyleAttr, defStylesRes);
         View view = inflate(context, R.layout.mobile_step_element, this);
 
-        TextView textView = view.findViewById(R.id.row_element_title);
+        TextView tvTitle = view.findViewById(R.id.row_element_title);
+        TextView tvSubText = view.findViewById(R.id.row_element_subtext);
+
         TypedArray a = context.getTheme().obtainStyledAttributes(
                 attributeSet,
                 R.styleable.MobileStepElement,
                 0, 0);
         try {
             String stepText = a.getString(R.styleable.MobileStepElement_stepTitle);
-            textView.setText(stepText);
+            String subText = a.getString(R.styleable.MobileStepElement_stepSubtext);
+
+            MobileStepIcon mobileStepIcon = MobileStepIcon.valueOf(a.getInteger(R.styleable.MobileStepElement_stepIcon, 0));
+            switch (mobileStepIcon) {
+                case ACTIVE:
+                    tvTitle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_empty_circle, 0, 0, 0);
+                    tvTitle.setTextAppearance(R.style.io_ta_stepper_form_style_active_step);
+                    break;
+                case INACTIVE:
+                    tvTitle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_inactive_circle, 0, 0, 0);
+                    tvTitle.setTextAppearance(R.style.io_ta_stepper_form_style_inactive_step);
+                    break;
+                case EDIT:
+                    tvTitle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_edit_circle, 0, 0, 0);
+                    tvTitle.setTextAppearance(R.style.io_ta_stepper_form_style_active_step);
+                    break;
+                case ERROR:
+                    tvTitle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_alert, 0, 0, 0);
+                    tvTitle.setTextAppearance(R.style.io_ta_stepper_form_style_active_step);
+                    break;
+                case CHECKED:
+                    tvTitle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_checkmark_circle_done, 0, 0, 0);
+                    tvTitle.setTextAppearance(R.style.io_ta_stepper_form_style_inactive_step);
+                    break;
+            }
+            tvTitle.setText(stepText);
+            if (subText != null) {
+                tvSubText.setVisibility(View.VISIBLE);
+                tvSubText.setText(subText);
+            }
         } finally {
             a.recycle();
         }
---
+++ library/src/main/java/io/tammen/stepper/widget/MobileStepIcon.java
@@ -0,0 +1,38 @@
+package io.tammen.stepper.widget;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Tammen Bruccoleri on 12/31/2017.
+ */
+
+public enum MobileStepIcon {
+    ACTIVE(0),
+    INACTIVE(1),
+    EDIT(2),
+    ERROR(3),
+    CHECKED(4);
+
+    private static Map map = new HashMap<>();
+
+    static {
+        for (MobileStepIcon mobileStepIcon : MobileStepIcon.values()) {
+            map.put(mobileStepIcon.value, mobileStepIcon);
+        }
+    }
+
+    private int value;
+
+    MobileStepIcon(int value) {
+        this.value = value;
+    }
+
+    public static MobileStepIcon valueOf(int mobileStepIcon) {
+        return (MobileStepIcon) map.get(mobileStepIcon);
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
